e6ebab70a86b3b04600bcde3b42b5e7fdb06c907
Anurag Negi <negianurag99@gmail.com>
2024-08-07 21:10:51 +0530

    Frog animation with controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
-   private void Update(){
+{   
+    private Animator animator;
+    private void Awake(){
+        animator = GetComponent<Animator>();
+    }
+    private void Update(){
+
     if(Input.GetKeyDown(KeyCode.W)){
         transform.position += new Vector3(0f, 1.0f, 0f);
+        animator.SetTrigger("forward");
+
     }
     if(Input.GetKeyDown(KeyCode.S)){
         transform.position -= new Vector3(0f, 1.0f, 0f);
+        animator.SetTrigger("Backward");
     }
     if(Input.GetKeyDown(KeyCode.A)){
         transform.position -= new Vector3(1.0f, 0f, 0f);
+        animator.SetTrigger("Left");
     }
     if(Input.GetKeyDown(KeyCode.D)){
         transform.position += new Vector3(1.0f, 0f, 0f);
+        animator.SetTrigger("Right");
     }
    }
 }
