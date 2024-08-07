f6d2af7a75318c0f4d8cd2220d02e9f950f0122f
Anurag Negi <negianurag99@gmail.com>
2024-08-07 12:06:12 +0530

    Player Basic Movemet

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+   private void Update(){
+    if(Input.GetKeyDown(KeyCode.W)){
+        transform.position += new Vector3(0f, 1.0f, 0f);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    if(Input.GetKeyDown(KeyCode.S)){
+        transform.position -= new Vector3(0f, 1.0f, 0f);
     }
+    if(Input.GetKeyDown(KeyCode.A)){
+        transform.position -= new Vector3(1.0f, 0f, 0f);
+    }
+    if(Input.GetKeyDown(KeyCode.D)){
+        transform.position += new Vector3(1.0f, 0f, 0f);
+    }
+   }
 }
