818a3968cadd07ca027f431373623701218c669e
Anurag Negi <negianurag99@gmail.com>
2024-08-08 13:00:39 +0530

    Water Detection Added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,26 +3,50 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {   
     private Animator animator;
+    [SerializeField] private LayerMask waterLayerMask;
+    [SerializeField] private bool inWater;
     private void Awake(){
         animator = GetComponent<Animator>();
     }
-    private void Update(){
+    private void Update()
+    {
+        CheckForWater();
+        PlayerMovement();
+    }
 
-    if(Input.GetKeyDown(KeyCode.W)){
-        transform.position += new Vector3(0f, 1.0f, 0f);
-        animator.SetTrigger("forward");
+    private void CheckForWater(){
+        float rayDistance = 0.5f;
+        Vector3 transformOffset = new Vector3(0f, 0.3f, 0f);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + transformOffset, transform.up, rayDistance, waterLayerMask);
+        if(hit){
+            inWater = true;
+        }else{
+            inWater = false;
+        }
+    Debug.DrawRay(transform.position + transformOffset, transform.up * rayDistance, Color.red, float.MaxValue);
+
     }
-    if(Input.GetKeyDown(KeyCode.S)){
-        transform.position -= new Vector3(0f, 1.0f, 0f);
-        animator.SetTrigger("Backward");
+    private void PlayerMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0f, 1.0f, 0f);
+            animator.SetTrigger("forward");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position -= new Vector3(0f, 1.0f, 0f);
+            animator.SetTrigger("Backward");
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position -= new Vector3(1.0f, 0f, 0f);
+            animator.SetTrigger("Left");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1.0f, 0f, 0f);
+            animator.SetTrigger("Right");
+        }
     }
-    if(Input.GetKeyDown(KeyCode.A)){
-        transform.position -= new Vector3(1.0f, 0f, 0f);
-        animator.SetTrigger("Left");
-    }
-    if(Input.GetKeyDown(KeyCode.D)){
-        transform.position += new Vector3(1.0f, 0f, 0f);
-        animator.SetTrigger("Right");
-    }
-   }
 }
